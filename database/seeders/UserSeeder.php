86265e10931ed3e6a8b85cbba6c6933c2f5a70f1
dikaproject <dikagilang2007@gmail.com>
2024-08-28 13:17:24 +0700

    up

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,37 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'name' => 'Pak Putra',
+            'email' => 'putra@smktelkom-pwt.sch.id',
+            'password' => bcrypt('Adminbuku123'),
+        ])->assignRole('admin');
 
+        Admin::create([
+            'nik' => '329889334424',
+            'name' => 'Leader',
+            'jabatan' => 'Ketua Tim Disiplin',
+            'sekolah' => 'SMK Telkom Purwokerto',
+            'email' => 'leader@smktelkom-pwt.sch.id',
+            'password' => bcrypt('password'),
+        ])->assignRole('leader');
+
+        Admin::create([
+            'nik' => '51238930033',
+            'name' => 'Teacher',
+            'jabatan' => 'Tim Disiplin',
+            'sekolah' => 'SMK Telkom Purwokerto',
+            'email' => 'teacher@smktelkom-pwt.sch.id',
+            'password' => bcrypt('password'),
+        ])->assignRole('teacher');
+
+        Admin::create([
+            'nik' => '512382332323',
+            'name' => 'Walas',
+            'jabatan' => 'Guru',
+            'sekolah' => 'SMK Telkom Purwokerto',
+            'email' => 'walas@smktelkom-pwt.sch.id',
+            'password' => bcrypt('password'),
+        ])->assignRole('walas');
     }
 }
